856b517c11432f923a9c382dbeaada1956146b57
minsu9605 <alstn9605@naver.com>
2021-10-26 12:40:30 +0900

    kakao login(토큰 받아오기)-인트라넷으로 인해 추가정보 못받아옴

---
+++ src/main/java/com/example/CUSHProject/controller/KakaoController.java
@@ -0,0 +1,42 @@
+package com.example.CUSHProject.controller;
+
+
+import com.example.CUSHProject.dto.OAuthToken;
+import com.example.CUSHProject.repository.MemberRepository;
+import com.example.CUSHProject.service.KakaoService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Controller
+public class KakaoController {
+    @Autowired
+    private KakaoService kakaoService;
+
+    @GetMapping(value="/kakao/oauth")
+    public String kakaoConnect() {
+
+        StringBuffer url = new StringBuffer();
+        url.append("https://kauth.kakao.com/oauth/authorize?");
+        url.append("client_id=f795106be2877118844d807a1e9b8cc9");
+        url.append("&redirect_uri=http://localhost:8080/kakao/callback");
+        url.append("&response_type=code");
+
+        return "redirect:" + url;
+    }
+
+        @GetMapping(value = "/kakao/callback")
+        public String kakaoOauthRedirect(@RequestParam String code,OAuthToken oauthToken, MemberRepository memberRepository) throws Exception {
+            OAuthToken oAuthToken = kakaoService.getAccessToken(code);
+            return kakaoService.getKakaoProfile(oAuthToken,memberRepository);
+            //return "index";
+        }
+}
