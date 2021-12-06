547ff30089d2273be4b7dfdd8c83020ce3ec3233
minsu9605 <alstn9605@naver.com>
2021-12-06 14:21:22 +0900

    no commit

--- src/test/java/com/example/CUSHProject/CushProjectApplicationTests.java
+++ src/test/java/com/example/CUSHProject/CushProjectApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.CUSHProject;
 import com.example.CUSHProject.job.OldBoardJobConfiguration;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -13,17 +14,21 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class CushProjectApplicationTests {
 
 	@Test
 	public void _테스트() {
-		//given
+		//Given
+		//테스트들을 위해 데이터들을 준비하는 과정
 
 
-		//when
+		//When
+		//실제로 용청을 하거나 싱행을 해보는 과정
 
 
-		//then
+		//Then
+		//실행한 결과를 검증하는 과정
 	}
 
 }
